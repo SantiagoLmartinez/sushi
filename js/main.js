11c4e82638609404f1f029c9be0208b5012a4e8b
Santiago L Martinez <slm17slm17@gmail.com>
2022-04-19 18:24:02 -0300

    se crea barra de navegacion

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('.burger')
+const navMenu = document.querySelector('.nav-menu')
+
+console.log('conectado')
+burger.addEventListener('click', () =>{
+    burger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+
+})
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () =>{
+    burger.classList.remove('active')
+    navMenu.classList.remove('active')
+
+}))
